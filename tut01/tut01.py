de09341e986c715338fa848591bfa889f79ccf9e
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-08-16 15:44:26 +0530

    add tut01 qn and soln

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,23 @@
-print("Hello, my name is Krishna. My Roll number is 1901EE50.")
+def meraki_helper(n: int) -> bool:
+    """This will detect meraki number"""
+    prev_dig = n % 10
+    n //= 10
+    while n:
+        curr_dig = n % 10
+        if abs(curr_dig - prev_dig) != 1:
+            return False
+        n //= 10
+    return True
+
+
+input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+
+meraki_count = 0
+for num in input:
+    if meraki_helper(num):
+        print("Yes,", num, "is a meraki number")
+        meraki_count += 1
+    else:
+        print("No,", num, "is not a meraki number")
+
+print("The input list contains", meraki_count, "meraki numbers and", len(input) - meraki_count, "non-meraki numbers")
