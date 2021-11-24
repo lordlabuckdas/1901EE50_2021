28c397f37de0fee1d1afa4a23391d7baeda4bd35
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-11-24 16:52:25 +0530

    [tut07] end sem assingment more comments

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -28,13 +28,13 @@ def init_courses():
     initialize COURSES dict with
     course code and the integer representation of its LTP
     """
-    global COURSES
     courses_file = pd.read_csv(COURSE_MASTER_FILE)
     for _, row in courses_file.iterrows():
-        ltp = str(row["ltp"]).split("-")
+        ltps = str(row["ltp"]).split("-")
         ltp_bits = 0
-        for i, b in enumerate(ltp):
-            ltp_bits |= (int(float(b)) and 1) << i
+        for idx in range(3):
+            if ltps[idx].strip() != '0':
+                ltp_bits |= 1 << idx
         COURSES[row["subno"]] = ltp_bits
 
 
@@ -43,7 +43,6 @@ def init_students():
     initialize STUDENTS dict with
     roll number and name, email, alternate email, contact number
     """
-    global STUDENTS
     students_file = pd.read_csv(STUDENT_FILE)
     for _, row in students_file.iterrows():
         STUDENTS[row["Roll No"]] = {
@@ -60,10 +59,9 @@ def init_reqd_feedback():
     key -> roll number + subject code
     val -> dict of register_sem and schedule_sem
     """
-    global REG_FEEDBACK
     reg_feedback_file = pd.read_csv(REGISTERED_COURSES_FILE)
     for _, row in reg_feedback_file.iterrows():
-        REG_FEEDBACK[str(row["rollno"]) + "-" + str(row["subno"])] = {
+        REG_FEEDBACK[str(row["subno"]) + "+" + str(row["rollno"])] = {
             "register_sem": row["register_sem"],
             "schedule_sem": row["schedule_sem"],
         }
@@ -75,15 +73,12 @@ def init_given_feedback():
     key -> roll number + subject code
     val -> integer representation of student's feedback LTP
     """
-    global GIVEN_FEEDBACK
     given_feedback_file = pd.read_csv(FEEDBACK_FILE)
     for _, row in given_feedback_file.iterrows():
-        GIVEN_FEEDBACK.setdefault(
-            str(row["stud_roll"]) + "-" + str(row["course_code"]), 0
-        )
-        GIVEN_FEEDBACK[
-            str(row["stud_roll"]) + "-" + str(row["course_code"])
-        ] |= (1 << int(row["feedback_type"]) - 1)
+        key = str(row["course_code"]) + "+" + str(row["stud_roll"]) 
+        if key not in GIVEN_FEEDBACK:
+            GIVEN_FEEDBACK[key] = 0
+        GIVEN_FEEDBACK[key] |= 1 << (int(row["feedback_type"]) - 1)
 
 
 def generate_missing_feedback():
@@ -91,7 +86,7 @@ def generate_missing_feedback():
     create output excel file in pandas
     if students' LTP bits do not match with required bits from course info
     """
-    global OUTPUT_FILE
+    # form empty dataframe with reqd columns
     remaining_feedback = pd.DataFrame(
         columns=[
             "rollno",
@@ -106,7 +101,8 @@ def generate_missing_feedback():
     )
     # iterate through given feedback
     for key, feedback_val in GIVEN_FEEDBACK.items():
-        roll, course = key.split("-")
+        # get back course and roll from key
+        course, roll = key.split("+")
         # handle case where LTP is 0-0-0
         if COURSES[course] not in (0, feedback_val):
             remaining_feedback = remaining_feedback.append(
@@ -131,10 +127,12 @@ def feedback_not_submitted():
     driver function to generate excel file of
     students who did not submit feedback
     """
+    # initialize all dicts
     init_courses()
     init_students()
     init_reqd_feedback()
     init_given_feedback()
+    # get details of students who didn't fill feedback
     generate_missing_feedback()
 
 
